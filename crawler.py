f78134eda2e3e3202277eb28c7ba9d3b463c91ef
Douglas Prado <douglas.prado@logicsp.com.br>
2018-12-14 10:20:16 -0200

    Added download zip file

--- crawler.py
+++ crawler.py
@@ -1,5 +1,31 @@
 import urllib.request
+import requests
 from urllib.error import URLError, HTTPError, ContentTooShortError
+
+RED = '\x1b[91m'
+RED1 = '\033[31m'
+BLUE = '\033[94m'
+GREEN = '\033[32m'
+BOLD = '\033[1m'
+NORMAL = '\033[0m'
+ENDC = '\033[0m'
+
+def file_download():
+    zip_file_url = "http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip"
+    print(GREEN + 'Downloading: {0}'.format(zip_file_url) + ENDC)
+    # r = requests.get(zip_file_url, stream=True)
+    try:
+        r = requests.get(zip_file_url, stream=True)
+        if r.status_code != 200:
+            print(RED + " * Error: Could not complete the download of the new version. Check your internet connection." + ENDC)
+            return False
+        with open('D_megase.zip', 'wb') as f:
+            f.write(r.content)
+
+        # z = ZipFile('D_megase.zip', 'r')
+    except (URLError, HTTPError, ContentTooShortError) as e:
+        print(RED + " * Error: Download error: ",e.reason + ENDC)
+
 
 def download(url, user_agent='wswp', num_retries=2):
     print('Downloading: {0} - Attempt: {1}'.format(url, (num_retries - num_retries) + 1))
@@ -16,7 +42,9 @@ def download(url, user_agent='wswp', num_retries=2):
                 return download(url, num_retries - 1)
     return html
 
-url = ""
-html = download(url)
-if (html != None):
-    print(html)
+# url = "http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip"
+# html = download(url,3)
+# if (html != None):
+#     print(html)
+
+file_download()
